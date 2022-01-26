640967a35bd68e7347d0caf810442c5279fc4064
Kvazikot <72801945+Kvazikot@users.noreply.github.com>
2022-01-26 12:59:18 +0300

    Update alwaystop.py

--- alwaystop.py
+++ alwaystop.py
@@ -1,8 +1,9 @@
 import win32gui
 import win32con
 import os
-
-os.system('notepad.exe')
-sleep(2)
+import time
+os.system('start notepad.exe')
+time.sleep(1)
 hwnd = win32gui.FindWindow('Notepad', None)
+print(hwnd)
 win32gui.SetWindowPos(hwnd, win32con.HWND_TOPMOST, 100, 100, 300, 200, 0) 
