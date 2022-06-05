851b0f557b61a4b5ad01969eba159de49d642bd1
Kvazikot <72801945+Kvazikot@users.noreply.github.com>
2022-06-05 20:09:35 +0300

    very important change

--- UsefulClicker/py/rect_gen_torch.py
+++ UsefulClicker/py/rect_gen_torch.py
@@ -15,60 +15,73 @@ from PIL import ImageFilter
 from PIL import Image, ImageDraw, ImageFont, ImageFilter
 import numpy as np
 
+alphabet = 'zxcvbnmasdfghjkqwertyui'
+
+def rand_string(min_l, max_l):
+    out=""
+    n_symbols = random.randint(min_l, max_l)
+    while n_symbols > 0:
+        n = random.randint(0, len(alphabet) )
+        out+=alphabet[n] 
+        n_symbols-=1
+    return out
+    
+     
+
 def gen_rect(image_size):
     canvas = Image.new('RGB', image_size, (255, 255, 255))
     draw = ImageDraw.Draw(canvas)
     w = random.randint(0, image_size[0])
     h = random.randint(0, image_size[0])
-    print(f'w={w} h={h}')
+    #print(f'w={w} h={h}')
     #draw.rectangle([0,0,w,h])
-    draw.ellipse([0,0,w,h],fill="rgb(25,30,150)", outline="rgb(255,255,150)", width=5)
-    canvas1 = canvas.filter(ImageFilter.GaussianBlur(3))
-    plt.imshow(canvas1)
+    r = 100 / random.randint(1, 10)  
+    canvas1 = canvas
+    print(r)
+    if r < 20:    
+        draw.ellipse([0,0,w,h],fill="rgb(25,30,150)", outline="rgb(255,255,150)", width=5)
+    if r > 20 and r < 80:
+        draw.rectangle([0,0,w,h],fill="rgb(25,30,150)", outline="rgb(255,255,150)", width=5)
+    if r > 80:
+        canvas1 = text_phantom(canvas, rand_string(1,10), image_size[0])
+    
+    canvas2 = canvas1.filter(ImageFilter.GaussianBlur(3))
+    #plt.imshow(canvas1)
     trs = transforms.ToTensor()
-    return trs( (255 - np.asarray(canvas)) / 255.0)
+    return trs( (255 - np.asarray(canvas2)) / 255.0)
 
-def text_phantom(text, size):
+def text_phantom(canvas, text, size):
     # Availability is platform dependent
     font = 'arial'
-    
     # Create font
-    pil_font = ImageFont.truetype(font + ".ttf", size=size // len(text),
-                                  encoding="unic")
-    
+    pil_font = ImageFont.truetype(font + ".ttf", size=size //2,
+                                  encoding="unic")   
     text_width, text_height = pil_font.getsize(text)
-
     # create a blank canvas with extra space between lines
-    canvas = Image.new('RGB', [size, size], (255, 255, 255))
-
     # draw the text onto the canvas
     draw = ImageDraw.Draw(canvas)
-    draw.ellipse([0,0,size/2,size/2],"rgb(255,0,0)" )
     offset = ((size - text_width) // 2,
               (size - text_height) // 2)
     white = "#0000AA"
     draw.text(offset, text, font=pil_font, fill=white)
-
     # Convert the canvas into an array with values in [0, 1]
-    return (255 - np.asarray(canvas)) / 255.0
+    #return (255 - np.asarray(canvas)) / 255.0
+    return canvas
 
 def show(img):
     npimg = img.numpy()
     plt.imshow(np.transpose(npimg, (1,2,0)), interpolation='nearest')
 
 w = torch.randn(10,3,200,640)
-trs = transforms.ToTensor()
-t1 = trs(text_phantom('dar',100))
-t2 = trs(text_phantom('ar',100))
 num_rects = 10
 image_size = [128,128]
 rect_tensors = []
 for i in range(0,num_rects,1):
-    t = gen_rect(image_size)    
+    t = gen_rect(image_size)            
     rect_tensors.append(t)
     
 #text_phantom()
 #plt.imshow(text_phantom('A', 200))
 #plt.imshow(text_phantom('Longer text', 200))
-grid = torchvision.utils.make_grid(rect_tensors, nrow=10, padding=10)
-#show(grid)
+grid = torchvision.utils.make_grid(rect_tensors, nrow=num_rects, padding=10)
+show(grid)
